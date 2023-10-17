78be82d4aa4daf957b9e05648095b0a12bde1ef1
Ivan Sidorov <nobxod@gmail.com>
2023-10-17 12:43:24 +0300

    Orders feature. Refactoring EditModal

--- resources/js/Pages/Orders/Index.vue
+++ resources/js/Pages/Orders/Index.vue
@@ -1,58 +1,25 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import {Head, router, useForm} from '@inertiajs/vue3';
+import {Head, router} from '@inertiajs/vue3';
 import NoItemsText from "@/Components/NoItemsText.vue";
 import {onMounted, ref} from "vue";
 import { BButton } from "bootstrap-vue-next";
-import EditModal from "@/Pages/Orders/Partials/EditModal.vue";
 import Pagination from "@/Components/Pagination.vue";
+import OrderEditModal from "@/Pages/Orders/Partials/OrderEditModal.vue";
 
 const props = defineProps({
     orders: Object,
     clients: Array,
 })
 
+/** @type OrderEditModal */
+const orderEditModal = ref(null);
+
 onMounted(() => {
     if (props.orders.data.length === 0 && props.orders.current_page > 1) {
         router.visit(props.orders.last_page_url);
     }
 })
-
-const modalOrderShown = ref(false);
-const modalOrderId = ref(null);
-const modalOrderForm = ref(useForm({
-    number: '',
-    date: '',
-    cost: '',
-    client_id: '',
-}));
-const modalOrderModel = ref(null);
-
-
-function newOrder() {
-    modalOrderForm.value = useForm({
-        number: '',
-        date: '',
-        cost: '',
-        client_id: '',
-    });
-    modalOrderId.value = null;
-    modalOrderShown.value = true;
-    modalOrderModel.value = null;
-}
-
-function editOrder(order) {
-    modalOrderForm.value = useForm({
-        number: order.number,
-        date: order.date,
-        cost: order.cost,
-        client_id: order.client.id,
-    });
-    modalOrderId.value = order.id;
-    modalOrderShown.value = true;
-    modalOrderModel.value = order;
-}
-
 
 </script>
 
@@ -63,7 +30,7 @@ function editOrder(order) {
         <template #header>
             <div class="d-flex justify-content-between">
                 <h2>Заказы</h2>
-                <BButton @click="newOrder()" variant="primary">Создать заказ</BButton>
+                <BButton @click="orderEditModal.showAsNew()" variant="primary">Создать заказ</BButton>
             </div>
         </template>
 
@@ -79,7 +46,7 @@ function editOrder(order) {
                 </thead>
 
                 <tbody>
-                <tr v-for="order in orders.data" :key="order.id" @click="editOrder(order)">
+                <tr v-for="order in orders.data" :key="order.id" @click="orderEditModal.showAsEdit(order)">
                     <td>{{ order.number }}</td>
                     <td>{{ order.date }}</td>
                     <td>{{ order.cost }}</td>
@@ -95,11 +62,5 @@ function editOrder(order) {
         </div>
     </AuthenticatedLayout>
 
-    <EditModal
-        v-model:form="modalOrderForm"
-        :id="modalOrderId"
-        v-model:show="modalOrderShown"
-        :order="modalOrderModel"
-        :clients="clients"
-    />
+    <OrderEditModal ref="orderEditModal" :clients="clients" />
 </template>
---
+++ resources/js/Pages/Orders/Partials/OrderEditModal.vue
@@ -0,0 +1,140 @@
+<template>
+    <EditModal v-model:show="show" :is-new="isNew" :title="title"
+               @init="resetForm()" @save="onSave" @delete="onDelete">
+
+        <TextInput
+            id="number"
+            :error="form.errors.number"
+            label="Номер"
+            v-model="form.number"
+            class="mb-3"
+        />
+
+        <TextInput
+            id="date"
+            :error="form.errors.date"
+            label="Дата"
+            v-model="form.date"
+            type="date"
+            class="mb-3"
+        />
+
+        <TextInput
+            id="cost"
+            :error="form.errors.cost"
+            label="Стоимость"
+            v-model="form.cost"
+            class="mb-3"
+        />
+
+        <BFormGroup
+            label="Клиент"
+            label-for="client_id"
+            :invalid-feedback="form.errors.client_id"
+            class="mb-3"
+        >
+            <BFormSelect id="client_id" v-model="form.client_id" :options="clientsList" />
+        </BFormGroup>
+
+        <template #deleteConfirm>
+            <p>
+                Вы действительно хотите удалить заказ номер "{{ order?.number }}" от {{ order?.date }}
+                клиента {{ order?.client.name }}?
+            </p>
+            <p>Восстановить данные после удаления невозможно.</p>
+        </template>
+    </EditModal>
+</template>
+
+<script setup>
+import EditModal from "@/Components/EditModal.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { BFormGroup, BFormSelect } from "bootstrap-vue-next";
+import {router, useForm} from "@inertiajs/vue3";
+import {computed, ref} from "vue";
+
+defineExpose({showAsNew, showAsEdit});
+
+const props = defineProps({
+    clients: {
+        type: Array,
+        required: true,
+    },
+});
+
+const show = ref(false);
+const id = ref(null);
+const order = ref(null);
+const form = ref(null);
+const processing = ref(false);
+
+const isNew = computed(() => !order.value);
+const title = computed(() => isNew.value ? 'Создание заказа' : 'Редактирование заказа');
+
+const clientsList = computed(()  => {
+    const clientIds = [];
+    const res = props.clients.map((client) => {
+        clientIds.push(client.id);
+        return {value: client.id, text: getClientText(client)};
+    });
+    if (order.value?.client?.id && !clientIds.includes(order.value.client.id)) {
+        // If exists client is soft-deleted
+        res.unshift({value: order.value.client.id, text: getClientText(order.value.client)})
+    }
+    return res;
+});
+
+function getClientText(client) {
+    return `${client.name} (${client.email})`;
+}
+
+function showAsNew() {
+    resetForm();
+    order.value = null;
+    id.value = null;
+    show.value = true;
+}
+
+function showAsEdit(editableOrder) {
+    resetForm(editableOrder);
+    order.value = editableOrder;
+    id.value = editableOrder.id;
+    show.value = true;
+}
+
+function resetForm(order = null) {
+    form.value = useForm({
+        number: order?.number || '',
+        date: order?.date || '',
+        cost: order?.cost || '',
+        client_id: order?.client?.id || '',
+    });
+}
+
+function onSave({onFinish}) {
+    const url = isNew.value ? route('order.new') : route('order.update', {id: order.value.id});
+    form.value.post(url, {
+        onSuccess() {
+            show.value = false;
+            if (isNew.value) {
+                router.visit(route('orders'));
+            }
+        },
+        onFinish,
+    });
+}
+
+function onDelete() {
+    const url = route('order.delete', {id: order.value.id});
+    router.visit(url, {
+        method: 'delete',
+        onSuccess() {
+            show.value = false;
+        },
+        onFinish() {
+            processing.value = false;
+        },
+    })
+}
+
+</script>
