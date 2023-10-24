c8f610fb7b330f0e11b0bc31b234ed9a9145ff9b
Ivan Sidorov <nobxod@gmail.com>
2023-10-24 19:37:36 +0300

    Minor fix

--- app/Http/Middleware/ValidateSignature.php
+++ app/Http/Middleware/ValidateSignature.php
@@ -11,7 +11,7 @@ class ValidateSignature extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
+    protected $ignore = [
         // 'fbclid',
         // 'utm_campaign',
         // 'utm_content',
